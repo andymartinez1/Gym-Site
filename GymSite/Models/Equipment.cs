3283f73dc7b31270ff846e73ec15f44c33eeb82f
Andrew Martinez <martinez.s.andrew@gmail.com>
2025-08-05 18:18:27 -0400

    Moving project from just React to C# ASP.NET Core MVC since this project will eventaully need a backend.

---
+++ GymSite/Models/Equipment.cs
@@ -0,0 +1,16 @@
+﻿namespace GymSite.Models;
+
+public class Equipment
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public int Quantity { get; set; }
+
+    public DateTime DateAdded { get; set; }
+
+    public bool InStock { get; set; }
+}
